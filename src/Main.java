880297c307ccb4c98800de9bece0dfc1e627218f
Oscar Vazquez Mier <ovazquez@elpuig.xeill.net>
2021-03-22 19:37:29 +0100

    no se Oscar me da igual

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,39 @@
+import control.Controller;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-
-        primaryStage.setScene(new Scene(root));
-        primaryStage.setResizable(false);
-
-        primaryStage.show();
-    }
-
-
     public static void main(String[] args) {
         launch(args);
     }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("sample/sample.fxml"));
+        StackPane root = loader.load();
+
+//SCENE TO WORK
+        Scene scene = new Scene(root);
+
+//Injectar Scene i Stage
+        Controller controller = loader.getController();
+        controller.setScene(scene);
+        controller.setStage(primaryStage);
+
+
+//CSS
+// http://fxexperience.com/2011/12/styling-fx-buttons-with-css/
+        scene.getStylesheets().add("css/samplecss.css");
+
+        primaryStage.setTitle("Rebenta globus");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
 }
--- src/control/Controller.java
+++ src/control/Controller.java
@@ -10,7 +10,7 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
-import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 import sprites.Globo;
 
@@ -19,8 +19,10 @@ import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
     private Scene scene;
+    private Stage stage;
     private GraphicsContext gc;
     private Globo globo;
+    private Globo globo2;
 
     @FXML
     Canvas mainCanvas;
@@ -32,13 +34,20 @@ public class Controller implements Initializable {
             globo.move();
             globo.render(gc);
 
+            globo2.clear(gc);
+            globo2.move();
+            globo2.render(gc);
+
+
         }
     })
     );
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        globo = new Globo(new Image("images/globo.png"));
+        globo = new Globo(new Image("css/images/globo_verde_grande.png"));
+        globo2 = new Globo(new Image("css/images/globo_azul.png"));
+
 
         gc = mainCanvas.getGraphicsContext2D();
         timeline.setCycleCount(Timeline.INDEFINITE);
@@ -46,4 +55,11 @@ public class Controller implements Initializable {
 
     }
 
+    public void setScene(Scene scene) {
+        this.scene = scene;
+    }
+
+    public void setStage(Stage primaryStage) {
+        this.stage = primaryStage;
+    }
 }
